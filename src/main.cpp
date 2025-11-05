9683dec117d14728805c515dcc2e39219205d236
Anthony Dudek <38356599+vegasdude@users.noreply.github.com>
2025-11-05 11:08:29 -0800

    Create src/main.cpp
    Hello from c++ project This is a modular C++ project

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "utils.hpp"
+
+int main() {
+    std::cout << "Hello from C++ project!" << std::endl;
+    printMessage("This is a modular C++ project.");
+    return 0;
+}
